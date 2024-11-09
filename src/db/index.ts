a6c071d357519b315d124e3cd373cefb11a08a18
sallyq1 <96536341+sallyq1@users.noreply.github.com>
2024-11-09 10:27:22 -0600

    more error fixes

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,3 +1,3 @@
 import { drizzle } from 'drizzle-orm/neon-http';
-const db = drizzle(process.env.DATABASE_URL);
+const db = drizzle("postgresql://fluentlydb_owner:a7u4pqTDHRfE@ep-weathered-glitter-a5w3yyp1.us-east-2.aws.neon.tech/fluentlydb?sslmode=require");
 
